25261058a2b59505abd1abfdfb608e007b1892a9
choemingyu <mchead1@naver.com>
2022-05-13 00:35:54 +0900

    :nut_and_bolt: 모델 객체 데이터클래스로 변경 및 필드 재정의

--- src/main/kotlin/com/example/helloworld/entity/StartupLog.kt
+++ src/main/kotlin/com/example/helloworld/entity/StartupLog.kt
@@ -2,9 +2,18 @@ package com.example.helloworld.entity
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
+import java.time.LocalDateTime
 
 @Document("startup_log")
 data class StartupLog(
     @Id
-    val _id: String
+    @Field("_id")
+    val id: String,
+    val buildinfo: Any,
+    val cmdLine: Any,
+    val hostname: String,
+    val pid: Int,
+    val startTime: LocalDateTime,
+    val startTimeLocal: String,
 )
