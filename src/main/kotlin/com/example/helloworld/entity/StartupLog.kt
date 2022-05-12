640804496450c0514e0aaada3fe6e37de3136f4f
choemingyu <mchead1@naver.com>
2022-05-13 00:15:55 +0900

    :nut_and_bolt: 모델객체 데이터 클래스로 변경

--- src/main/kotlin/com/example/helloworld/entity/StartupLog.kt
+++ src/main/kotlin/com/example/helloworld/entity/StartupLog.kt
@@ -1,6 +1,10 @@
 package com.example.helloworld.entity
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document("startup_log")
-class StartupLog(val _id: String) {}
+data class StartupLog(
+    @Id
+    val _id: String
+)
