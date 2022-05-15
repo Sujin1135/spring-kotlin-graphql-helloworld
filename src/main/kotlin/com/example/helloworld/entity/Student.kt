6b6064183d860ac96c6b7a02dadd7539f6c4ccf5
choemingyu <mchead1@naver.com>
2022-05-15 22:32:34 +0900

    :fire: uninstall expedia graphql libraries

--- src/main/kotlin/com/example/helloworld/entity/Student.kt
+++ src/main/kotlin/com/example/helloworld/entity/Student.kt
@@ -3,17 +3,12 @@ package com.example.helloworld.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.Field
-import java.time.LocalDateTime
 
-@Document("startup_log")
-data class StartupLog(
+@Document("students")
+data class Student (
     @Id
     @Field("_id")
     val id: String,
-//    val buildinfo: Any,
-//    val cmdLine: Any,
-    val hostname: String,
-    val pid: Int,
-//    val startTime: LocalDateTime,
-    val startTimeLocal: String,
+    val city: String,
+    val fname: String,
 )
