100a4f024dfce2f052520641b5bb5389804dada4
choemingyu <mchead1@naver.com>
2022-05-15 23:05:38 +0900

    :sparkles: install dgs and create sample

---
+++ src/main/kotlin/com/example/helloworld/datafetcher/StartupLogDataFetcher.kt
@@ -0,0 +1,42 @@
+package com.example.helloworld.datafetcher
+
+import com.example.helloworld.entity.Student
+import com.example.helloworld.repository.StartupLogRepository
+import com.example.helloworld.repository.StudentRepository
+import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsQuery
+import com.netflix.graphql.dgs.InputArgument
+import org.springframework.beans.factory.annotation.Autowired
+import reactor.core.publisher.Flux
+
+@DgsComponent
+class StartupLogDataFetcher {
+    @Autowired
+    lateinit var repository: StudentRepository
+
+    private val shows = listOf(
+        Show("Stranger Things", 2016),
+        Show("Ozark", 2017),
+        Show("The Crown", 2016),
+        Show("Dead to Me", 2019),
+        Show("Orange is the New Black", 2013),
+    )
+
+    @DgsQuery
+    fun shows(@InputArgument titleFilter : String?): List<Show> {
+        println("*** called findAllStartupLogs")
+
+        return if(titleFilter != null) {
+            shows.filter { it.title.contains(titleFilter) }
+        } else {
+            shows
+        }
+    }
+
+    data class Show(val title: String, val releaseYear: Int)
+
+    @DgsQuery
+    fun students(@InputArgument titleFilter : String?): Flux<Student> {
+        return repository.findAll()
+    }
+}
--- src/main/kotlin/com/example/helloworld/entity/StartupLog.kt
+++ src/main/kotlin/com/example/helloworld/entity/StartupLog.kt
@@ -3,17 +3,12 @@ package com.example.helloworld.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.Field
-import java.time.LocalDateTime
 
 @Document("startup_log")
 data class StartupLog(
     @Id
     @Field("_id")
     val id: String,
-//    val buildinfo: Any,
-//    val cmdLine: Any,
     val hostname: String,
     val pid: Int,
-//    val startTime: LocalDateTime,
-    val startTimeLocal: String,
 )
