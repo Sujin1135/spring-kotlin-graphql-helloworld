89f9049139fff2d121cb0ee79e2ed59363a0cdb2
choemingyu <mchead1@naver.com>
2022-05-17 23:45:28 +0900

    :sparkles: graphql 조회 데이터타입 추가(array, object)

--- src/main/kotlin/com/example/helloworld/entity/StartupLog.kt
+++ src/main/kotlin/com/example/helloworld/entity/StartupLog.kt
@@ -11,4 +11,44 @@ data class StartupLog(
     val id: String,
     val hostname: String,
     val pid: Int,
+    @Field("buildinfo")
+    val buildInfo: BuildInfo
 )
+
+data class BuildInfo(
+    val debug: Boolean,
+    val gitVersion: String,
+    val javascriptEngine: String,
+    val maxBsonObjectSize: Int,
+    val sysInfo: String,
+    val version: String,
+    val versionArray: Array<String>,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BuildInfo
+
+        if (debug != other.debug) return false
+        if (gitVersion != other.gitVersion) return false
+        if (javascriptEngine != other.javascriptEngine) return false
+        if (maxBsonObjectSize != other.maxBsonObjectSize) return false
+        if (sysInfo != other.sysInfo) return false
+        if (version != other.version) return false
+        if (!versionArray.contentEquals(other.versionArray)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = debug.hashCode()
+        result = 31 * result + gitVersion.hashCode()
+        result = 31 * result + javascriptEngine.hashCode()
+        result = 31 * result + maxBsonObjectSize
+        result = 31 * result + sysInfo.hashCode()
+        result = 31 * result + version.hashCode()
+        result = 31 * result + versionArray.contentHashCode()
+        return result
+    }
+}
